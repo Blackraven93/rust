e43070b4648c6506ff4a352f3fff2778c40c84f2
Blackraven93 <reblackraven@gmail.com>
2023-01-04 23:36:24 +0900

    feat(if): add if let printer

--- src/enum.rs
+++ src/enum.rs
@@ -106,4 +106,14 @@ fn value_in_cents(coin: Coin) -> u32 {
       Coin::Dime => 10,
       Coin::Quarter => 25,
   }
+
+  let some_u8_value = Some(0u8);
+  //   match some_u8_value {
+  //     Some(3) => println!("three"),
+  //     _ => (),
+  // }
+  
+  if let Some(3) = some_u8_value {
+    println!("three");
+  }
 }
